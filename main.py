b4410a32a6d00e5d1cb609dc49b5c8f0bea7b4a9
RuslanPC <onishruslan@yandex.ru>
2023-06-14 22:31:43 +0300

    minor changes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ bot = telebot.TeleBot("6113316955:AAH9zbWFe1lzDPyj6H57MMoJ9Y0B_w34S_Y")
 def audio(message):
     file = open('voice.mp3', 'rb')
     bot.send_audio(message.chat.id, file)
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку audio')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку audio')
 
 
 @bot.message_handler(commands=["start"])
@@ -22,10 +22,10 @@ def main(message):
     markup.row(btn1)
     bot.send_message(message.chat.id, f"Привет {message.from_user.first_name} {message.from_user.username}!\nЯ бот помощник, напиши /help",reply_markup=markup)
     bot.register_next_step_handler(message, on_click_help)
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку start')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку start')
 
 def on_click_help(message):
-    if message.text == "Помощь по командам":
+    if message.text == "Помощь по командам" or "/help":
         help1(message)
 
 
@@ -35,7 +35,7 @@ def help1(message):
     markup.add(types.InlineKeyboardButton('Перейти на сайт', url="https://ober1.st8.ru/"))
     bot.send_message(message.chat.id, "<b>Information:</b>\n/start - Старт бота\n/id - Узнать ваш id в telegram\n/website - Открыть сайт\n/photo - Я отправлю вам фото\n/audio - Я отправлю вам аудио\nТак-же ты можешь отправить мне фото!",
                      parse_mode='html', reply_markup=markup)
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку help')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку help')
 
 
 @bot.message_handler(commands=["photo"])
@@ -45,7 +45,7 @@ def send_photo(message):
     markup.row(btn1)
     file = open(f'flower{random.randint(1,3)}.png', 'rb')
     bot.send_photo(message.chat.id, file, reply_markup=markup)
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку удалить фото')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку фото')
 
 
 
@@ -53,13 +53,13 @@ def send_photo(message):
 @bot.message_handler(commands=["id"])
 def id(message):
     bot.reply_to(message, f'Ваш id : {message.from_user.id}')
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку id')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку id')
 
 
 @bot.message_handler(commands=["website","site"])
 def website(message):
     webbrowser.open("https://ober1.st8.ru/")
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку site')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку site')
 
 
 @bot.message_handler(content_types=["photo"])
@@ -70,11 +70,11 @@ def get_photo(message):
     markup.row(btn1, btn2)
     markup.add()
     bot.reply_to(message, "Какое красивое фото!", reply_markup=markup)
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) отправил фото')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) отправил фото')
 
 @bot.message_handler()
 def info(message):
-    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) отправил сообщение id:{message.message_id-1} text:{message.text}')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) отправил сообщение id:{message.message_id-1} text:{message.text}')
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Здарова")
     elif message.text == "Помощь по командам":
@@ -91,5 +91,6 @@ def callback(callback):
     elif callback.data == 'delete_last':
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         bot.send_message(callback.message.chat.id, "Фото удалено!")
+        print(f'{datetime.now()}: Пользователь {message.from_user.first_name} (id:{message.from_user.id}) Нажал на кнопку удалить фото')
 
 bot.polling(none_stop=True)
