376dc59bc8c9fd9ef49e7f6ad75e78f4f93803ae
RuslanPC <onishruslan@yandex.ru>
2023-06-14 21:57:10 +0300

    add function 'send audio' and add logs in console

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-
+import time
+from datetime import datetime
 import telebot
 import webbrowser
 from telebot import types
@@ -7,6 +8,11 @@ from telebot import types
 bot = telebot.TeleBot("6113316955:AAH9zbWFe1lzDPyj6H57MMoJ9Y0B_w34S_Y")
 
 
+@bot.message_handler(commands=["audio"])
+def audio(message):
+    file = open('voice.mp3', 'rb')
+    bot.send_audio(message.chat.id, file)
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку audio')
 
 
 @bot.message_handler(commands=["start"])
@@ -16,6 +22,7 @@ def main(message):
     markup.row(btn1)
     bot.send_message(message.chat.id, f"Привет {message.from_user.first_name} {message.from_user.username}!\nЯ бот помощник, напиши /help",reply_markup=markup)
     bot.register_next_step_handler(message, on_click_help)
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку start')
 
 def on_click_help(message):
     if message.text == "Помощь по командам":
@@ -26,8 +33,9 @@ def on_click_help(message):
 def help1(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Перейти на сайт', url="https://ober1.st8.ru/"))
-    bot.send_message(message.chat.id, "<b>Information:</b>\n/start - Старт бота\n/id - Узнать ваш id в telegram\n/website - Открыть сайт\nТак-же ты можешь отправить мне фото!\n/photo - Я отправлю вам фото",
+    bot.send_message(message.chat.id, "<b>Information:</b>\n/start - Старт бота\n/id - Узнать ваш id в telegram\n/website - Открыть сайт\n/photo - Я отправлю вам фото\n/audio - Я отправлю вам аудио\nТак-же ты можешь отправить мне фото!",
                      parse_mode='html', reply_markup=markup)
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку help')
 
 
 @bot.message_handler(commands=["photo"])
@@ -37,6 +45,7 @@ def send_photo(message):
     markup.row(btn1)
     file = open(f'flower{random.randint(1,3)}.png', 'rb')
     bot.send_photo(message.chat.id, file, reply_markup=markup)
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку удалить фото')
 
 
 
@@ -44,11 +53,13 @@ def send_photo(message):
 @bot.message_handler(commands=["id"])
 def id(message):
     bot.reply_to(message, f'Ваш id : {message.from_user.id}')
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку id')
 
 
 @bot.message_handler(commands=["website","site"])
 def website(message):
     webbrowser.open("https://ober1.st8.ru/")
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) Нажал на кнопку site')
 
 
 @bot.message_handler(content_types=["photo"])
@@ -59,10 +70,11 @@ def get_photo(message):
     markup.row(btn1, btn2)
     markup.add()
     bot.reply_to(message, "Какое красивое фото!", reply_markup=markup)
-
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) отправил фото')
 
 @bot.message_handler()
 def info(message):
+    print(f'{datetime.now()}: Пользователь {message.from_user.first_name} ({message.from_user.id}) отправил сообщение id:{message.message_id-1} text:{message.text}')
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Здарова")
     elif message.text == "Помощь по командам":
