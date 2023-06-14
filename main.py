a72e73f8bc541fb8e60219bffd035ed656b4d4aa
RuslanPC <onishruslan@yandex.ru>
2023-06-14 21:33:04 +0300

    add function 'send random photo'

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import telebot
 import webbrowser
 from telebot import types
@@ -20,13 +22,23 @@ def on_click_help(message):
         help1(message)
 
 
-
 @bot.message_handler(commands=["help"])
 def help1(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Перейти на сайт', url="https://ober1.st8.ru/"))
-    bot.send_message(message.chat.id, "<b>Information:</b>\n/start - Старт бота\n/id - Узнать ваш id в telegram\n/website - Открыть сайт\nТак-же ты можешь отправить мне фото!",
+    bot.send_message(message.chat.id, "<b>Information:</b>\n/start - Старт бота\n/id - Узнать ваш id в telegram\n/website - Открыть сайт\nТак-же ты можешь отправить мне фото!\n/photo - Я отправлю вам фото",
                      parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(commands=["photo"])
+def send_photo(message):
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    btn1 = types.InlineKeyboardButton('Удалить фото', callback_data='delete_last')
+    markup.row(btn1)
+    file = open(f'flower{random.randint(1,3)}.png', 'rb')
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+
+
 
 
 @bot.message_handler(commands=["id"])
@@ -53,6 +65,8 @@ def get_photo(message):
 def info(message):
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, "Здарова")
+    elif message.text == "Помощь по командам":
+        help1(message)
     else:
         bot.send_message(message.chat.id, 'Такой команды нет. /help - чтобы узнать команды')
 
@@ -62,6 +76,8 @@ def callback(callback):
         bot.delete_message(callback.message.chat.id, callback.message.message_id-1)
     elif callback.data == 'edit':
         bot.edit_message_text("Какое красивое фото!",callback.message.chat.id, callback.message.message_id)
-
+    elif callback.data == 'delete_last':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        bot.send_message(callback.message.chat.id, "Фото удалено!")
 
 bot.polling(none_stop=True)
