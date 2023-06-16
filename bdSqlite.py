35ef5422dccafe398bbd97ff1cef6408e9362482
RuslanPC <onishruslan@yandex.ru>
2023-06-16 22:36:13 +0300

    работает авторизация

--- bdSqlite.py
+++ bdSqlite.py
@@ -7,7 +7,10 @@ bot = telebot.TeleBot("6113316955:AAH9zbWFe1lzDPyj6H57MMoJ9Y0B_w34S_Y")
 login = None
 id = None
 auth_process = None
-
+account = None
+user_input_login = None
+user_input_password = None
+user_input_login = None
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Привет.\n/auth - авторизация\n/reg - регистрация')
@@ -18,7 +21,6 @@ def reg_user(message):
     conn = sqlite3.connect('ober.sql')
     cur = conn.cursor()
     id = message.from_user.id
-    print(id)
     cur.execute('CREATE TABLE IF NOT EXISTS users (id int(50), login varchar(50), pass varchar(50))')
     conn.commit()
     cur.close()
@@ -27,13 +29,21 @@ def reg_user(message):
     bot.send_message(message.chat.id, 'привет, сейчас тебя зарегистрируем, придумайте логин')
     bot.register_next_step_handler(message, user_login)
 
-
-
+@bot.message_handler(commands=['auth'])
+def auth_user(message):
+    global auth_process
+    auth_process = True
+    try:
+        bot.send_message(message.chat.id, 'НЕ ВВОДИТЕ НИКАКИЕ КОМАНДЫ В ПРОЦЕССЕ АВТОРИЗАЦИИ!\nВведите Логин:')
+        bot.register_next_step_handler(message, login_auth)
+    except:
+        pass
 
 def login_auth(message):
     global auth_process
+    global user_input_login
     if not '/' in message.text:
-        print(message.text)
+        user_input_login = (message.text)
         bot.send_message(message.chat.id, 'Введите пароль:')
         bot.register_next_step_handler(message, password_auth)
     else:
@@ -42,12 +52,38 @@ def login_auth(message):
 
 
 def password_auth(message):
+    global account
     global auth_process
+    global user_input_password
     if not '/' in message.text:
-        print(message.text)
+        user_input_password = (message.text)
+
     else:
-        bot.send_message('Не используйте символ /')
+        bot.send_message(message.chat.id, 'Не используйте символ /')
         auth_process = False
+        return
+    conn = sqlite3.connect('ober.sql')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users')
+    users = cur.fetchall()
+
+    for i in users:
+        print(i[1], user_input_login)
+        print(i[2], user_input_password)
+        if i[1] == user_input_login:
+            if i[2] == user_input_password:
+                account = i[0]
+                cur.close()
+                conn.close()
+                bot.send_message(message.chat.id, f'Добро пожаловать, {i[1]} (id:{message.from_user.id})')
+                auth_process = False
+                return
+    bot.send_message(message.chat.id, 'Не удачно.')
+    cur.close()
+    conn.close()
+
+
+
 
 
 
